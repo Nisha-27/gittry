1a87ba765c0b3df5368b98036fd3c0c316034b3c
Nisha-27 <nisharavi2712@gmail.com>
2021-11-11 11:46:13 +0530

    commit

--- src/com/git/demo/demo1.java
+++ src/com/git/demo/demo1.java
@@ -4,6 +4,13 @@ public class demo1 {
 	public void code1()
 	{
 		System.out.println("Hi");
+	
+		System.out.println("check");
+	}
+	
+	public void code3()
+	{
+		System.out.println("Hello");
 	}
 
 }
--- src/com/git/demo/demo2.java
+++
@@ -1,9 +0,0 @@
-package com.git.demo;
-
-public class demo2 {
-	public void code2()
-	{
-		System.out.println("Hi");
-	}
-
-}
