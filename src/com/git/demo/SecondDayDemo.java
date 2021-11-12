5afc56c80440df4429f64438680e51ffab6b2c62
Nisha-27 <nisharavi2712@gmail.com>
2021-11-12 14:49:37 +0530

    second day commit

---
+++ src/com/git/demo/SecondDayDemo.java
@@ -0,0 +1,16 @@
+package com.git.demo;
+
+public class SecondDayDemo {
+	public void code5()
+	{
+		System.out.println("Hi");
+	
+		System.out.println("check");
+	}
+	
+	public void code6()
+	{
+		System.out.println("Hello");
+	}
+
+}
